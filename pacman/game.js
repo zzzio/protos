ee217d9aecf610484227dd78d64e3a0798195eb2
Aether Lazuli <admin@athelo.net>
2017-11-03 18:03:17 +0000

    Create game.js

---
+++ pacman/game.js
@@ -0,0 +1,23 @@
+$(document).ready(function){
+ console.log('Jquery has loaded!')
+ 
+ var
+ 
+ $(document).on('keydown', handleTyping) {
+
+  function handleTyping(event) {
+    
+    switch(event.which){
+    
+       case 39;
+         $('#pacman').css({
+         
+         })
+     
+     }
+    
+    }
+  
+  }
+
+})
